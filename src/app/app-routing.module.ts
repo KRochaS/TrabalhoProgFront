a284aa4113b3ea6837cc145ddc38a079bc53f5c5
KRochaS <karinerocha.s@hotmail.com>
2020-08-23 16:58:46 -0300

    Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { FramePageComponent } from './frame-page/frame-page.component';
 import { LoginComponent } from './account/login/login.component';
 import { SignupComponent } from './account/signup/signup.component';
-import { ResetPasswordComponent } from './account/reset-password/reset-password.component';
 import { ContaComponent } from './account/conta/conta.component';
 import { ApoieComponent } from './apoie/apoie.component';
 import { InformacoesComponent } from './informacoes/informacoes.component';
@@ -18,7 +17,6 @@ const routes: Routes = [
       { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
-      { path: 'reset-password', component: ResetPasswordComponent },
       { path: 'conta', component: ContaComponent},
       { path: 'apoie', component: ApoieComponent},
       {path: 'informacoes', component: InformacoesComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ToasterModule } from 'angular2-toaster';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { ContaComponent } from './account/conta/conta.component';
 import { LoginComponent } from './account/login/login.component';
 import { SignupComponent } from './account/signup/signup.component';
-import { ResetPasswordComponent } from './account/reset-password/reset-password.component';
-import { HomeComponent } from './home/home.component';
-import { FramePageComponent } from './frame-page/frame-page.component';
-import { ContaComponent } from './account/conta/conta.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {ToasterModule} from 'angular2-toaster';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { ApoieComponent } from './apoie/apoie.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FramePageComponent } from './frame-page/frame-page.component';
+import { HomeComponent } from './home/home.component';
 import { InformacoesComponent } from './informacoes/informacoes.component';
-
-export const environment = {
-  firebase: {
-    apiKey: "AIzaSyBG7A6u7Jx0wBzkHFUUzrw30sh3LbyZtzI",
-    authDomain: "infofibro-45cb8.firebaseapp.com",
-    databaseURL: "https://infofibro-45cb8.firebaseio.com",
-    projectId: "infofibro-45cb8",
-    storageBucket: "infofibro-45cb8.appspot.com",
-    messagingSenderId: "125956159970",
-    appId: "1:125956159970:web:896b2c2dd911d80a50a1bb",
-    measurementId: "G-72EB1R23HK"
-  }
-};
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +21,6 @@ export const environment = {
     NavbarComponent,
     LoginComponent,
     SignupComponent,
-    ResetPasswordComponent,
     HomeComponent,
     FramePageComponent,
     ContaComponent,
