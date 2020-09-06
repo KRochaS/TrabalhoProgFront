37933cee02e2e40ed59e18c6f05bfb4fca521a09
KRochaS <karinerocha.s@hotmail.com>
2020-09-06 20:04:19 -0300

    fix url

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND =  window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: 'https://info-fibro.herokuapp.com/';
+: 'https://info-fibro.herokuapp.com';
 
 
 export default {
